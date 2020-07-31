f635e258e0d836cdf3d8ddd131237e05273a8037
zhangml123 <s15532292192@163.com>
2020-07-31 17:16:00 +0800

    Add files via upload

---
+++ db/db_conn.js
@@ -0,0 +1,40 @@
+'use strict'
+var mysql = require('mysql');
+
+class db_conn{
+	constructor(){
+		this.connection;
+	}
+	db_connect(){
+		var self = this;
+		self.option = {
+		  host     : 'HOST',
+		  user     : 'ROOT',
+		  password : 'PASSWORD',
+		  database : 'DB'
+		}
+		self.connection = mysql.createConnection(self.option);
+		self.connection.connect(function(e){
+			console.log("connecting mysql")
+		});
+		self.connection.on("error",function(e){
+			console.log("db error ",e);
+			if (e.code === 'PROTOCOL_CONNECTION_LOST') {
+				var connection = mysql.createConnection(self.option);
+				connection.connect()
+				self.connection = connection
+	        } else {
+	            throw e;
+	        }
+		})
+		var DB = require("./db_function");
+		var error = this.connectionErrorException;
+		var db = new DB(self.connection,error);
+		return db;
+	}
+	connectionErrorException(e,db_func){
+		console.log("reconnecting to mysql server")
+	}
+}
+
+module.exports = db_conn;
